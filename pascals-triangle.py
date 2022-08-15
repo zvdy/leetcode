dc9f6ec28aa82f5573819f95def17bcf45b76886
zvdy <cristian.030902@gmail.com>
2022-08-15 10:18:57 +0200

    pascals triangle

---
+++ pascals-triangle.py
@@ -0,0 +1,16 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        if numRows == 0:
+            return []
+        if numRows == 1:
+            return [[1]]
+        if numRows == 2:
+            return [[1], [1, 1]]
+        res = [[1], [1, 1]]
+        for i in range(2, numRows):
+            row = [1]
+            for j in range(1, i):
+                row.append(res[i-1][j-1] + res[i-1][j])
+            row.append(1)
+            res.append(row)
+        return res
