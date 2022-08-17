60324e30ef84930b3c7769644310c3c5eadafc0f
zvdy <cristian.030902@gmail.com>
2022-08-17 19:36:33 +0200

    search a 3d matrix II

---
+++ search-a-2d-matrix-II.py
@@ -0,0 +1,14 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if not matrix:
+            return False
+        row = 0
+        col = len(matrix[0]) - 1
+        while row < len(matrix) and col >= 0:
+            if matrix[row][col] == target:
+                return True
+            elif matrix[row][col] > target:
+                col -= 1
+            else:
+                row += 1
+        return False
