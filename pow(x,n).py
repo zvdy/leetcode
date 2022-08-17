c36bea29f2d072b1f7e2dbc8ec743128c775354f
zvdy <cristian.030902@gmail.com>
2022-08-17 19:54:54 +0200

    pow(x, n)

---
+++ pow(x,n).py
@@ -0,0 +1,14 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+  def myPow(self, x: float, n: int) -> float:
+    if n == 0:
+      return 1
+    if n < 0:
+      return 1 / self.myPow(x, -n)
+    if n % 2:
+      return x * self.myPow(x, n - 1)
+    return self.myPow(x * x, n / 2)
