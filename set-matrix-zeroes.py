b9b09e7fb25fb1cc84c6d82b2775cd07e3c0a2cf
zvdy <cristian.030902@gmail.com>
2022-08-17 18:52:39 +0200

    set matrix zeroes

---
+++ set-matrix-zeroes.py
@@ -0,0 +1,21 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        if not matrix:
+            return
+        m = len(matrix)
+        n = len(matrix[0])
+        row = [False] * m
+        col = [False] * n
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == 0:
+                    row[i] = True
+                    col[j] = True
+        for i in range(m):
+            for j in range(n):
+                if row[i] or col[j]:
+                    matrix[i][j] = 0
+        return matrix
