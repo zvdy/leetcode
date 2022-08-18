3e6ba277985c230a35c96aa742c90ddc0a201c55
zvdy <cristian.030902@gmail.com>
2022-08-18 16:29:32 +0200

    task scheduler

---
+++ task-scheduler.py
@@ -0,0 +1,7 @@
+class Solution:
+  def leastInterval(self, tasks: List[str], n: int) -> int:
+    count = Counter(tasks)
+    maxFreq = max(count.values())
+    maxFreqTaskOccupy = (maxFreq - 1) * (n + 1)
+    nMaxFreq = sum(value == maxFreq for value in count.values())
+    return max(maxFreqTaskOccupy + nMaxFreq, len(tasks))
