d2505f367a880f3e86a03084bdaf628c12b83235
zvdy <cristian.030902@gmail.com>
2022-08-20 14:24:17 +0200

    sum of left leaves

---
+++ sum-of-left-leaves.py
@@ -0,0 +1,12 @@
+class Solution:
+    def sumOfLeftLeaves(self, root: TreeNode) -> int:
+        if root == None:
+            return 0
+        ans = 0
+        if root.left != None:
+            if root.left.left == None and root.left.right == None:
+                ans += root.left.val
+            else:
+                ans += self.sumOfLeftLeaves(root.left)
+        ans += self.sumOfLeftLeaves(root.right)
+        return ans
