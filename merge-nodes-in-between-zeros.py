d68eb1fd8d07be9d6f371b00a4354f2073c2892f
zvdy <cristian.030902@gmail.com>
2022-08-23 22:26:25 +0200

    merge nodes in between zeros

---
+++ merge-nodes-in-between-zeros.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+  def mergeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    if not head:
+      return None
+    if not head.next.val:
+      node = ListNode(head.val)
+      node.next = self.mergeNodes(head.next.next)
+      return node
+
+    next = self.mergeNodes(head.next)
+    next.val += head.val
+    return next
