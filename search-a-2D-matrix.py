6e3dad11e8acff3752f8784ca7d5bf3f4e406692
zvdy <cristian.030902@gmail.com>
2022-08-25 21:20:18 +0200

    search a 2D matrix

---
+++ search-a-2D-matrix.py
@@ -0,0 +1,8 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if not matrix:
+            return False
+        for row in matrix:
+            if target in row:
+                return True
+        return False
