cca8d7e7940890828909a59ddb24ec884094a00b
zvdy <cristian.030902@gmail.com>
2022-08-27 12:08:05 +0200

    reverse words in a string III

---
+++ reverse-words-in-a-string-III.py
@@ -0,0 +1,11 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        s = s.split(" ")
+        s1 = ""
+        for i in s:
+            count = len(i)
+            s1 += " "
+            while count > 0:
+                s1 += i[count - 1]
+                count -= 1
+        return s1.strip()
