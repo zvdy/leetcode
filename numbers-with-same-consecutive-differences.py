b52a72a66cca98908a957f21e711e3a12d56d8f8
zvdy <cristian.030902@gmail.com>
2022-09-03 18:10:50 +0200

    numbers with same consecutive differences

---
+++ numbers-with-same-consecutive-differences.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numsSameConsecDiff(self, n: int, k: int) -> List[int]:
+        res = []
+        q = deque((1, d) for d in range(1, 10))
+        while q:
+            pos, num = q.pop() 
+            if pos == n:    
+                res.append(num) 
+            else:
+                for j in range(10): 
+                    if abs(num % 10 - j) == k:  
+                        q.append((pos + 1, num * 10 + j))        
+        return res
