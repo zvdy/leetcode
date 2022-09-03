31833c41c28fc1f78024f60c8a010eedc8211283
zvdy <cristian.030902@gmail.com>
2022-09-03 18:16:10 +0200

    letter case permutation

---
+++ letter-case-permutation.py
@@ -0,0 +1,15 @@
+class Solution:
+    def letterCasePermutation(self, s: str) -> List[str]:
+        res = []
+        q = deque([(0, '')])
+        while q:
+            pos, perm = q.pop()
+            if pos == len(s):
+                res.append(perm)
+            else:
+                if s[pos].isalpha():
+                    q.append((pos + 1, perm + s[pos].lower()))
+                    q.append((pos + 1, perm + s[pos].upper()))
+                else:
+                    q.append((pos + 1, perm + s[pos]))
+        return res
