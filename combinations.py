78f770f0af78caccb12277ff66a28e67d2cd70bb
zvdy <cristian.030902@gmail.com>
2022-09-03 18:15:18 +0200

    combinations

---
+++ combinations.py
@@ -0,0 +1,13 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        def f(i,res):
+            if(len(res)==k):
+                ans.append(res[:])
+            for i in range(i,n+1):
+                res.append(i)
+                f(i+1,res)
+                res.pop()
+            return ans
+        ans=[]
+        f(1,[])
+        return ans  
