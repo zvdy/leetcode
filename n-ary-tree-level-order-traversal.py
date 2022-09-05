510d99abcdbedf574eb82a4b7ac8aabb3a4e604f
zvdy <cristian.030902@gmail.com>
2022-09-05 21:27:19 +0200

    n-ary tree level order traversal

---
+++ n-ary-tree-level-order-traversal.py
@@ -0,0 +1,18 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def levelOrder(self, root: 'Node') -> List[List[int]]:
+        if not root:
+            return []
+        res = []
+        queue = [root]
+        while queue:
+            res.append([node.val for node in queue])
+            queue = [child for node in queue for child in node.children]
+        return res
