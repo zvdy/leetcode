d39b595e93b09f7654b8f1aab2dba2e80388db29
zvdy <cristian.030902@gmail.com>
2022-09-10 19:50:04 +0200

    4sum

---
+++ 4sum.py
@@ -0,0 +1,27 @@
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        nums.sort()
+        result = []
+        for i in range(len(nums)-3):
+            if i>0 and nums[i]==nums[i-1]:
+                continue
+            for j in range(i+1,len(nums)-2):
+                if j>i+1 and nums[j]==nums[j-1]:
+                    continue
+                left = j+1
+                right = len(nums)-1
+                while left<right:
+                    sum4 = nums[i]+nums[j]+nums[left]+nums[right]
+                    if sum4==target:
+                        result.append([nums[i],nums[j],nums[left],nums[right]])
+                        left+=1
+                        right-=1
+                        while left<right and nums[left]==nums[left-1]:
+                            left+=1
+                        while left<right and nums[right]==nums[right+1]:
+                            right-=1
+                    elif sum4>target:
+                        right-=1
+                    else:
+                        left+=1
+        return result
