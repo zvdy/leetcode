482f4f6bb216c02940f3a472be5eb4d12a7db5c1
zvdy <cristian.030902@gmail.com>
2022-09-10 19:44:19 +0200

    zigzag conversion

---
+++ zigzag-conversion.py
@@ -0,0 +1,12 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1:
+            return s
+        rows = [''] * numRows
+        i, flag = 0, -1
+        for c in s:
+            rows[i] += c
+            if i == 0 or i == numRows - 1:
+                flag = -flag
+            i += flag
+        return ''.join(rows)
