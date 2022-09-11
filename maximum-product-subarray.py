e049e26a83074f4287d0266e2e6b93a2602f997e
zvdy <cristian.030902@gmail.com>
2022-09-11 12:55:26 +0200

    maximum product subarray

---
+++ maximum-product-subarray.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        res = max(nums)
+        currMin, currMax = 1, 1
+
+        for n in nums:
+            if n == 0:
+                currMin, currMax = 1, 1
+                continue
+        
+            tmp = currMax * n
+            currMax = max(n * currMax, n * currMin, n)
+            currMin = min(tmp, n * currMin, n)
+            res = max(res, currMax)
+            
+        return res
