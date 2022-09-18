4e3b2610c4dd320e9b71b8ea60e4408a4396e36c
zvdy <cristian.030902@gmail.com>
2022-09-18 21:38:41 +0200

    combination sum II

---
+++ combination-sum-II.py
@@ -0,0 +1,16 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        result = []
+        def dfs(remain, stack, index):
+            if remain == 0:
+                result.append(stack)
+                return
+            for i in range(index, len(candidates)):
+                if i > index and candidates[i] == candidates[i - 1]:
+                    continue
+                if candidates[i] > remain:
+                    break
+                dfs(remain - candidates[i], stack + [candidates[i]], i + 1)
+        dfs(target, [], 0)
+        return result
