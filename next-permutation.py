37c321af450999ccfe05c0692debec8f49b1832d
zvdy <cristian.030902@gmail.com>
2022-09-18 21:36:39 +0200

    next permutation

---
+++ next-permutation.py
@@ -0,0 +1,16 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        if len(nums) <= 1:
+            return
+        i = len(nums) - 2
+        while i >= 0 and nums[i] >= nums[i + 1]:
+            i -= 1
+        if i >= 0:
+            j = len(nums) - 1
+            while j >= 0 and nums[j] <= nums[i]:
+                j -= 1
+            nums[i], nums[j] = nums[j], nums[i]
+        nums[i + 1:] = nums[i + 1:][::-1]
