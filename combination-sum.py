474a792e1e17f6478349b6111ba0121cf4f438b3
zvdy <cristian.030902@gmail.com>
2022-09-18 21:37:28 +0200

    combination sum

---
+++ combination-sum.py
@@ -0,0 +1,16 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        result = []
+        def dfs(remain, stack):
+            if remain == 0:
+                result.append(stack)
+                return
+            for item in candidates:
+                if item > remain:
+                    break
+                if stack and item < stack[-1]:
+                    continue
+                dfs(remain - item, stack + [item])
+        dfs(target, [])
+        return result
