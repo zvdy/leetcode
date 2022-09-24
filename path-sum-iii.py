92540d153853064e663efd2fdba88ccf3a88aac5
zvdy <cristian.030902@gmail.com>
2022-09-24 11:03:53 +0200

    path sum III

---
+++ path-sum-iii.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:        
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+        if root is None:
+            return []
+        q, paths = deque([(root, targetSum, [])]), []
+        
+        while q:
+            cur, target, path = q.pop()  
+            if not (cur.left or cur.right) and cur.val == target:
+                paths.append(path + [cur.val])
+            else:
+                if cur.left:
+                    q.appendleft((cur.left, target - cur.val, path + [cur.val]))
+                if cur.right:
+                    q.appendleft((cur.right, target - cur.val, path + [cur.val]))
+                                 
+        return paths
