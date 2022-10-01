0ac6338c1389cd95ca245cba61d79e2fbe753cea
zvdy <cristian.030902@gmail.com>
2022-10-01 20:55:54 +0200

    decode ways

---
+++ decode-ways.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        if s[0] == '0':
+            return 0
+        dp = [0] * (len(s) + 1)
+        dp[0] = 1
+        dp[1] = 1
+        for i in range(2, len(s) + 1):
+            if s[i - 1] != '0':
+                dp[i] += dp[i - 1]
+            if s[i - 2] == '1' or (s[i - 2] == '2' and s[i - 1] < '7'):
+                dp[i] += dp[i - 2]
+        return dp[len(s)]
