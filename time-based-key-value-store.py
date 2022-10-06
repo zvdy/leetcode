fa7c52455beb33ed8b466059003218fece686765
zvdy <cristian.030902@gmail.com>
2022-10-06 10:22:46 +0200

    time based key value store

---
+++ time-based-key-value-store.py
@@ -0,0 +1,20 @@
+class TimeMap:
+  def __init__(self):
+    self.values = defaultdict(list)
+    self.timestamps = defaultdict(list)
+
+  def set(self, key: str, value: str, timestamp: int) -> None:
+    self.values[key].append(value)
+    self.timestamps[key].append(timestamp)
+
+  def get(self, key: str, timestamp: int) -> str:
+    if key not in self.timestamps:
+      return ''
+    i = bisect.bisect(self.timestamps[key], timestamp)
+    return self.values[key][i - 1] if i > 0 else ''
+
+
+# Your TimeMap object will be instantiated and called as such:
+# obj = TimeMap()
+# obj.set(key,value,timestamp)
+# param_2 = obj.get(key,timestamp)
