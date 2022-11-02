1c62904ba9e51113e5e0c49455e2896b0b53bd48
zvdy <cristian.030902@gmail.com>
2022-11-02 12:55:41 +0100

    minimum genetic mutation

---
+++ minimum-genetic-mutation.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minMutation(self, start: str, end: str, bank: List[str]) -> int:
+        if end not in bank:
+            return -1
+        bank = set(bank)
+        queue = deque([(start, 0)])
+        while queue:
+            gene, level = queue.popleft()
+            if gene == end:
+                return level
+            for i in range(len(gene)):
+                for c in "ACGT":
+                    new_gene = gene[:i] + c + gene[i+1:]
+                    if new_gene in bank:
+                        bank.remove(new_gene)
+                        queue.append((new_gene, level+1))
+        return -1
