6091fa3902a4ad5e21b5d181660a35d03d0b1ce0
zvdy <cristian.030902@gmail.com>
2022-11-05 19:53:46 +0100

    word search

--- word-search.py
+++ word-search.py
@@ -1,51 +1,25 @@
-class TrieNode:
-  def __init__(self):
-    self.children: Dict[str, TrieNode] = defaultdict(TrieNode)
-    self.word: Optional[str] = None
-
-
 class Solution:
-  def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+  def exist(self, board: List[List[str]], word: str) -> bool:
     m = len(board)
     n = len(board[0])
-    ans = []
-    root = TrieNode()
 
-    def insert(word: str) -> None:
-      node = root
-      for c in word:
-        if c not in node.children:
-          node.children[c] = TrieNode()
-        node = node.children[c]
-      node.word = word
-
-    for word in words:
-      insert(word)
-
-    def dfs(i: int, j: int, node: TrieNode) -> None:
+    def dfs(i: int, j: int, s: int) -> bool:
       if i < 0 or i == m or j < 0 or j == n:
-        return
-      if board[i][j] == '*':
-        return
+        return False
+      if board[i][j] != word[s] or board[i][j] == '*':
+        return False
+      if s == len(word) - 1:
+        return True
 
-      c = board[i][j]
-      if c not in node.children:
-        return
-
-      child = node.children[c]
-      if child.word:
-        ans.append(child.word)
-        child.word = None
-
+      cache = board[i][j]
       board[i][j] = '*'
-      dfs(i + 1, j, child)
-      dfs(i - 1, j, child)
-      dfs(i, j + 1, child)
-      dfs(i, j - 1, child)
-      board[i][j] = c
+      isExist = \
+          dfs(i + 1, j, s + 1) or \
+          dfs(i - 1, j, s + 1) or \
+          dfs(i, j + 1, s + 1) or \
+          dfs(i, j - 1, s + 1)
+      board[i][j] = cache
 
-    for i in range(m):
-      for j in range(n):
-        dfs(i, j, root)
+      return isExist
 
-    return ans
+    return any(dfs(i, j, 0) for i in range(m) for j in range(n))
