d1a236b2fdd1f577e5d21880fb832012f5b04e40
zvdy <cristian.030902@gmail.com>
2022-11-21 08:07:03 +0100

    word break II

---
+++ word-break-II.py
@@ -0,0 +1,14 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        if not s:
+            return []
+        if not wordDict:
+            return []
+        dp = [[] for _ in range(len(s) + 1)]
+        dp[0] = [""]
+        for i in range(1, len(s) + 1):
+            for j in range(i):
+                if dp[j] and s[j:i] in wordDict:
+                    for word in dp[j]:
+                        dp[i].append(word + " " + s[j:i] if word else s[j:i])
+        return dp[-1]
