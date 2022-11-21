fe10bc5d46988a08a89ddb04b414548f10109ec7
zvdy <cristian.030902@gmail.com>
2022-11-21 08:06:18 +0100

    word break

---
+++ word-break.py
@@ -0,0 +1,14 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        if not s:
+            return True
+        if not wordDict:
+            return False
+        dp = [False] * (len(s) + 1)
+        dp[0] = True
+        for i in range(1, len(s) + 1):
+            for j in range(i):
+                if dp[j] and s[j:i] in wordDict:
+                    dp[i] = True
+                    break
+        return dp[-1]
