9a17df7bada187a9f39b27c4eaa26959329fef4b
zvdy <cristian.030902@gmail.com>
2022-11-30 09:48:36 +0100

    unique number of occurrences

---
+++ unique-number-of-occurrences.py
@@ -0,0 +1,9 @@
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        d = {}
+        for i in arr:
+            if i in d:
+                d[i] += 1
+            else:
+                d[i] = 1
+        return len(d) == len(set(d.values()))
