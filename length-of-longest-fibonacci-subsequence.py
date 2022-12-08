d7c5ed83fc1397affdf255c5cea7cdfd4fc8e27a
zvdy <cristian.030902@gmail.com>
2022-12-08 22:44:05 +0100

    lenght of longest fibonacci subsequence

---
+++ length-of-longest-fibonacci-subsequence.py
@@ -0,0 +1,12 @@
+class Solution:
+    def lenLongestFibSubseq(self, arr: List[int]) -> int:
+        n = len(arr)
+        dp = [[2] * n for _ in range(n)]
+        ans = 0
+        for i in range(n):
+            for j in range(i + 1, n):
+                k = bisect.bisect_left(arr, arr[i] + arr[j])
+                if k < n and arr[k] == arr[i] + arr[j]:
+                    dp[j][k] = dp[i][j] + 1
+                    ans = max(ans, dp[j][k])
+        return ans if ans >= 3 else 0
