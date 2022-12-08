80f43832d04d3d4551761b9d8259f60418cc03c0
zvdy <cristian.030902@gmail.com>
2022-12-08 22:46:30 +0100

    koko eating bananas

---
+++ koko-eating-bananas.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        def canFinish(speed):
+            return sum((pile - 1) // speed + 1 for pile in piles) <= h
+        left, right = 1, max(piles)
+        while left < right:
+            mid = (left + right) // 2
+            if canFinish(mid):
+                right = mid
+            else:
+                left = mid + 1
+        return left
