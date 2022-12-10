fdd6aab962d4f167d4c438fe552328b2d788c5c1
zvdy <cristian.030902@gmail.com>
2022-12-10 22:20:31 +0100

    maximum product of splitted binary tree

---
+++ maximum-product-of-splitted-binary-tree.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxProduct(self, root: Optional[TreeNode]) -> int:
+        def dfs(node):
+            if not node:
+                return 0
+            return node.val + dfs(node.left) + dfs(node.right)
+        total = dfs(root)
+        ans = 0
+        def dfs2(node):
+            nonlocal ans
+            if not node:
+                return 0
+            left = dfs2(node.left)
+            right = dfs2(node.right)
+            ans = max(ans, left * (total - left), right * (total - right))
+            return node.val + left + right
+        dfs2(root)
+        return ans % (10 ** 9 + 7)
