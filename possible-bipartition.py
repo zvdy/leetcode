1a122529ddddc4ae51f3cd19dcda900180ed85ea
zvdy <cristian.030902@gmail.com>
2022-12-21 22:57:32 +0100

    possible bipartition

---
+++ possible-bipartition.py
@@ -0,0 +1,13 @@
+class Solution:
+    def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
+        graph = collections.defaultdict(set)
+        for u, v in dislikes:
+            graph[u].add(v)
+            graph[v].add(u)
+        color = {}
+        def dfs(node, c = 0):
+            if node in color:
+                return color[node] == c
+            color[node] = c
+            return all(dfs(nei, c ^ 1) for nei in graph[node])
+        return all(dfs(node) for node in range(1, n + 1) if node not in color)
