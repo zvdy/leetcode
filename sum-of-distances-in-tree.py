ba0517768247515d8670df68e197de045884c63e
zvdy <cristian.030902@gmail.com>
2022-12-22 22:40:27 +0100

    sum of distances in tree

---
+++ sum-of-distances-in-tree.py
@@ -0,0 +1,29 @@
+class Solution:
+    def sumOfDistancesInTree(self, n: int, edges: List[List[int]]) -> List[int]:
+        g = collections.defaultdict(list)
+        for u, v in edges:
+            g[u].append(v)
+            g[v].append(u)
+        
+        d = {i:[1, 0] for i in range(n)}
+        
+        def dfs(root, prev):
+            for nei in g[root]:
+                if nei != prev:
+                    dfs(nei, root)
+                    d[root][0] += d[nei][0]
+                    d[root][1] += (d[nei][0] + d[nei][1])
+        
+        def dfs2(root, prev):
+            for nei in g[root]:
+                if nei != prev:
+                    d[nei][1] = d[root][1] - d[nei][0] + (n-d[nei][0])
+                    dfs2(nei, root)
+        
+        dfs(0, -1)
+        dfs2(0, -1)
+        res = []
+        for key in d:
+            res.append(d[key][1])
+        return res
+        
