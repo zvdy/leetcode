86cd58128044f2cf4edb9165adbed949bc37ed9b
zvdy <cristian.030902@gmail.com>
2022-12-24 21:13:20 +0100

    domino and tromino tiling

---
+++ domino-and-tromino-tiling.py
@@ -0,0 +1,18 @@
+class Solution:
+    def numTilings(self, n: int) -> int:
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        if n == 3:
+            return 5
+        dp = [0] * (n + 1)
+        dp[0] = 0
+        dp[1] = 1
+        dp[2] = 2
+        dp[3] = 5
+        for i in range(4, n + 1):
+            dp[i] = (2 * dp[i - 1] + dp[i - 3]) % (10 ** 9 + 7)
+        return dp[n]
