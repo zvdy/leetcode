4a0fcb967b8a6e2fb4df1048be27f6cd5dd36f9a
zvdy <cristian.030902@gmail.com>
2022-12-24 21:13:57 +0100

    custom sort string

---
+++ custom-sort-string.py
@@ -0,0 +1,12 @@
+class Solution:
+    def customSortString(self, order: str, s: str) -> str:
+        if not order:
+            return s
+        if not s:
+            return s
+        order_map = {}
+        for i in range(len(order)):
+            order_map[order[i]] = i
+        s = list(s)
+        s.sort(key=lambda x: order_map.get(x, len(order)))
+        return ''.join(s)
