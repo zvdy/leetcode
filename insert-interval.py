e5b64c080b91d2c5ad401028699a1bb0c0ccb356
zvdy <cristian.030902@gmail.com>
2022-12-26 20:04:41 +0100

    insert interval

---
+++ insert-interval.py
@@ -0,0 +1,13 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        if not intervals:
+            return [newInterval]
+        intervals.append(newInterval)
+        intervals.sort(key=lambda x: x[0])
+        result = [intervals[0]]
+        for i in range(1, len(intervals)):
+            if result[-1][1] >= intervals[i][0]:
+                result[-1][1] = max(result[-1][1], intervals[i][1])
+            else:
+                result.append(intervals[i])
+        return result
