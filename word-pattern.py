7338354b3652c79ef895bb4e7e89ad979f38c096
zvdy <cristian.030902@gmail.com>
2023-01-01 18:56:53 +0100

    word pattern

---
+++ word-pattern.py
@@ -0,0 +1,15 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        words = s.split()
+        if len(pattern) != len(words):
+            return False
+        d = {}
+        for i in range(len(pattern)):
+            if pattern[i] not in d:
+                if words[i] in d.values():
+                    return False
+                d[pattern[i]] = words[i]
+            else:
+                if d[pattern[i]] != words[i]:
+                    return False
+        return True
