d1794c286768c9ed1ebe47912c97531ae55c56e4
zvdy <cristian.030902@gmail.com>
2023-01-04 21:29:48 +0100

    minimum rounds to complete all tasks

---
+++ minimum-rounds-to-complete-all-tasks.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minimumRounds(self, tasks: list[int]) -> int:
+        tasks = Counter(tasks)                      
+        if 1 in tasks.values(): return -1 
+        ans = 0                                  
+        for n in tasks.values():
+            ans+= n//3 + bool(n%3)          
+        return  ans
