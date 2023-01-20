6b5b99845d73edfb3c17d0ec2dd91e87c2193e60
zvdy <cristian.030902@gmail.com>
2023-01-20 18:37:38 +0100

    non decreasing subsequences

---
+++ non-decreasing-subsequences.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findSubsequences(self, nums: List[int]) -> List[List[int]]:
+        res = set()
+        def dfs(nums, path):
+            if len(path) >= 2:
+                res.add(tuple(path))
+            for i in range(len(nums)):
+                if not path or nums[i] >= path[-1]:
+                    dfs(nums[i+1:], path + [nums[i]])
+        dfs(nums, [])
+        return res
