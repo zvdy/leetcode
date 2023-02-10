76e43fb117d05ed0c33b637b8116609968942f5d
zvdy <cristian.030902@gmail.com>
2023-02-10 21:10:25 +0100

    as far from land as possible

---
+++ as-far-from-land-as-possible.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxDistance(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        q = deque()
+        for i in range(n):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    q.append((i, j))
+        if len(q) == 0 or len(q) == n * n:
+            return -1
+        res = 0
+        while q:
+            res += 1
+            for _ in range(len(q)):
+                i, j = q.popleft()
+                for x, y in [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]:
+                    if 0 <= x < n and 0 <= y < n and grid[x][y] == 0:
+                        grid[x][y] = 1
+                        q.append((x, y))
+        return res - 1
