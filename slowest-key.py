23eb8bdd921b2bd35cf9fb328a8f12805f0a07b8
zvdy <cristian.030902@gmail.com>
2023-02-20 22:31:02 +0100

    slowest key

---
+++ slowest-key.py
@@ -0,0 +1,13 @@
+class Solution:
+    def slowestKey(self, releaseTimes: List[int], keysPressed: str) -> str:
+        max_duration = releaseTimes[0]
+        max_key = keysPressed[0]
+        for i in range(1, len(releaseTimes)):
+            duration = releaseTimes[i] - releaseTimes[i-1]
+            if duration > max_duration:
+                max_duration = duration
+                max_key = keysPressed[i]
+            elif duration == max_duration:
+                if keysPressed[i] > max_key:
+                    max_key = keysPressed[i]
+        return max_key
