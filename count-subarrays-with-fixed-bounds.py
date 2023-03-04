6c64444f9df4135d2faa416077fdb58d986a0a75
zvdy <cristian.030902@gmail.com>
2023-03-04 21:23:10 +0100

    count subarrays with fixed bounds

---
+++ count-subarrays-with-fixed-bounds.py
@@ -0,0 +1,24 @@
+class Solution:
+    def countSubarrays(self, nums: List[int], minK: int, maxK: int) -> int:
+        
+        max_index, min_index = -1,-1
+        count = 0
+        i,restart = 0,0
+
+        while i<len(nums):
+            if nums[i]<minK or nums[i]>maxK:
+                restart = i+1
+                max_index, min_index = -1,-1
+            if nums[i]==minK:
+                min_index = i
+            if nums[i]==maxK:
+                max_index = i
+
+        
+            c = 1 + min(max_index,min_index) - restart
+            count += c if c>=0 else 0
+
+            i += 1
+        
+        return count
+
