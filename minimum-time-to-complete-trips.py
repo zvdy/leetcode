e403ac6528c5557149b662eb2b2842a4efb62743
zvdy <cristian.030902@gmail.com>
2023-03-07 16:55:12 +0100

    minimum time to complete trips

---
+++ minimum-time-to-complete-trips.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minimumTime(self, time: List[int], totalTrips: int) -> int:
+        def check(t):
+            trips = 0
+            for i in range(len(time)):
+                trips += t // time[i]
+                if trips >= totalTrips:
+                    return True
+            return False
+        
+        l, r = 0, 10**18
+        while l < r:
+            m = (l + r) // 2
+            if check(m):
+                r = m
+            else:
+                l = m + 1
+        return l
