634560736c697f56390a687e16492e015655bf15
zvdy <cristian.030902@gmail.com>
2023-03-21 21:04:03 +0100

    number of zero filled subarrays

---
+++ number-of-zero-filled-subarrays.py
@@ -0,0 +1,15 @@
+class Solution:
+    def zeroFilledSubarray(self, nums: List[int]) -> int:
+        count = 0
+        res = 0
+        for num in nums:
+            if num==0:
+                count+=1
+            else:
+                res+=count*(count+1)//2
+                count = 0
+        if count:
+            res+=count*(count+1)//2
+        return res
+
+            
