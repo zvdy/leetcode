1ca5f56046f76c30d6db2de803a6901405873ec2
zvdy <cristian.030902@gmail.com>
2023-03-23 19:33:01 +0100

    number of operations to make network connected

---
+++ number-of-operations-to-make-network-connected.py
@@ -0,0 +1,18 @@
+class Solution:
+    def makeConnected(self, n: int, connections: List[List[int]]) -> int:
+        if len(connections)<n-1:
+            return -1
+        parent = [i for i in range(n)]
+        def find(x):
+            if parent[x]!=x:
+                parent[x] = find(parent[x])
+            return parent[x]
+        def union(x,y):
+            parent[find(x)] = find(y)
+        for x,y in connections:
+            union(x,y)
+        res = 0
+        for i in range(n):
+            if parent[i]==i:
+                res+=1
+        return res-1
