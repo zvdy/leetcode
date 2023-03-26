062adfe1b8d2c15b9ea91afc0f76f4b06f7c9a50
zvdy <cristian.030902@gmail.com>
2023-03-27 00:05:24 +0200

    longest cycle in a graph

---
+++ longest-cycle-in-a-graph.py
@@ -0,0 +1,31 @@
+class Solution:
+    def longestCycle(self, edges: List[int]) -> int:
+        n = len(edges)
+        visited = [False] * n
+        max = -1
+        for i in range(n):
+            if visited[i]:
+                continue
+            if edges[i] == -1:
+                visited[i] = True
+                continue
+            route = set()
+            cur = i
+            while edges[cur] != -1:
+                visited[cur] = True
+                route.add(cur)
+                cur = edges[cur]
+                if visited[cur] and cur not in route:
+                    break
+                if cur in route:
+                    start = cur
+                    count = 1
+                    cur = edges[cur]
+                    while cur != start:
+                        count += 1
+                        cur = edges[cur]
+                    if count > max:
+                        max = count
+                    break
+        return max
+                    
