22b70a3c4e48f352671ade69c11e56c6b206d89a
zvdy <cristian.030902@gmail.com>
2023-03-29 23:10:35 +0200

    reducing dishes

---
+++ reducing-dishes.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxSatisfaction(self, satisfaction: List[int]) -> int:
+        satisfaction.sort()
+        n = len(satisfaction)
+        dp = [0] * (n + 1)
+        for i in range(n - 1, -1, -1):
+            for j in range(i, n):
+                dp[j + 1] = max(dp[j + 1], dp[j] + satisfaction[i] * (j - i + 1))
+        return dp[-1]
