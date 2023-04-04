fccd2c4d5ee92870f94ef39d78c3ef0845fae1fb
zvdy <cristian.030902@gmail.com>
2023-04-04 20:11:29 +0200

    optimal partition of string

---
+++ optimal-partition-of-string.py
@@ -0,0 +1,35 @@
+class Solution:
+    def partitionString(self, s: str) -> int:
+        
+        # initialize the result
+        result = 0
+
+        # initialize the set of characters
+        char_set = set()
+
+        # iterate through the characters in the string
+        for char in s:
+
+            # if the character is not in the set
+            if char not in char_set:
+
+                # add the character to the set
+                char_set.add(char)
+
+            # otherwise
+            else:
+
+                # increment the result
+                result += 1
+
+                # reset the set
+                char_set = set()
+
+                # add the character to the set
+                char_set.add(char)
+
+        # increment the result
+        result += 1
+
+        # return the result
+        return result
