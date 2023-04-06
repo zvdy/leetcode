4b0ba164d0b4a7c4fbe7059e1b5b21e2bc9bf6a9
zvdy <cristian.030902@gmail.com>
2023-04-06 16:29:37 +0200

    number of closed islands

---
+++ number-of-closed-islands.py
@@ -0,0 +1,23 @@
+class Solution:
+    def closedIsland(self, grid: List[List[int]]) -> int:
+        def dfs(i, j):
+            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == 1:
+                return
+            grid[i][j] = 1
+            dfs(i - 1, j)
+            dfs(i + 1, j)
+            dfs(i, j - 1)
+            dfs(i, j + 1)
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if (i == 0 or i == len(grid) - 1 or j == 0 or j == len(grid[0]) - 1) and grid[i][j] == 0:
+                    dfs(i, j)
+
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 0:
+                    dfs(i, j)
+                    count += 1
+        return count
