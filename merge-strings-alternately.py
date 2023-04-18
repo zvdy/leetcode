2846056b8794b128a132bba18533c39bf6b5743b
zvdy <cristian.030902@gmail.com>
2023-04-18 22:40:24 +0200

    merge strings alternately

---
+++ merge-strings-alternately.py
@@ -0,0 +1,12 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        s = ""
+        i = 0
+        while i < len(word1) and i < len(word2):
+            s += word1[i] + word2[i]
+            i += 1
+        if i < len(word1):
+            s += word1[i:]
+        if i < len(word2):
+            s += word2[i:]
+        return s
