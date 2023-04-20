30f6017856177c840c44f704b4b2d40ed33fb0ca
zvdy <cristian.030902@gmail.com>
2023-04-20 23:24:39 +0200

    maximum width of binary tree

---
+++ maximum-width-of-binary-tree.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        queue = deque([(root, 0)])
+        max_width = 0
+        while queue:
+            size = len(queue)
+            _, left = queue[0]
+            _, right = queue[-1]
+            max_width = max(max_width, right - left + 1)
+            for _ in range(size):
+                node, index = queue.popleft()
+                if node.left:
+                    queue.append((node.left, 2 * index))
+                if node.right:
+                    queue.append((node.right, 2 * index + 1))
+        return max_width
