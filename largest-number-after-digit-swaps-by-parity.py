74dd4cd451b6778e3a771f557b0212b91d6d48e3
zvdy <cristian.030902@gmail.com>
2023-04-27 22:29:31 +0200

    largest number after digit swaps by parity

---
+++ largest-number-after-digit-swaps-by-parity.py
@@ -0,0 +1,28 @@
+class Solution:
+    def largestInteger(self, num: int) -> int:
+
+        mapping_dict = {str(i) : i for i in range(0, 10)}
+
+        odd = []
+        even = []
+
+        str_num = str(num)
+        for char in str_num:
+            if mapping_dict[char] % 2 == 0:
+                even.append(mapping_dict[char])
+            else:
+                odd.append(mapping_dict[char])
+        odd.sort(); even.sort()
+
+        integer = 0; odd_count = 0; even_count = 0
+        for idx, char in enumerate(str_num[::-1]):
+            if mapping_dict[char] % 2 == 0:
+                integer += (pow(10, idx) * even[even_count]) 
+                even_count += 1
+            else:
+                integer += (pow(10, idx) * odd[odd_count]) 
+                odd_count += 1
+        
+        return integer
+
+
