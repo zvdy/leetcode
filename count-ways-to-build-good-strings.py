d5418a8df4195b5a94295a1b63a723b7815a9fbf
zvdy <cristian.030902@gmail.com>
2023-05-13 23:43:32 +0200

    count ways to build good strings

---
+++ count-ways-to-build-good-strings.py
@@ -0,0 +1,16 @@
+class Solution:
+    def countGoodStrings(self, low: int, high: int, zero: int, one: int) -> int:
+        mod = 10 ** 9 + 7
+        cache = {}
+
+        def dfs(length):
+            if length > high:
+                return 0
+            if length in cache:
+                return cache[length]
+            
+            cache[length] = 1 if length >= low else 0
+            cache[length] += dfs(length + zero) + dfs(length + one)
+            return cache[length] % mod
+        
+        return dfs(0)
