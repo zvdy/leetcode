6708804e59d6b7df01cb3636e645fc5302b75a8b
zvdy <cristian.030902@gmail.com>
2023-05-16 21:59:59 +0200

    swapping nodes in a linked list

--- swapping-nodes-in-a-linked-list.py
+++ swapping-nodes-in-a-linked-list.py
@@ -4,21 +4,10 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def swapNodes(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        # Find the length of the linked list
-        n = 0
-        cur = head
-        while cur:
-            n += 1
-            cur = cur.next
-        # Find the kth node from the start
-        kth_from_start = head
-        for _ in range(k-1):
-            kth_from_start = kth_from_start.next
-        # Find the kth node from the end
-        kth_from_end = head
-        for _ in range(n-k):
-            kth_from_end = kth_from_end.next
-        # Swap the values
-        kth_from_start.val, kth_from_end.val = kth_from_end.val, kth_from_start.val
-        return head
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        node = head.next
+        head.next = self.swapPairs(node.next)
+        node.next = head
+        return node
