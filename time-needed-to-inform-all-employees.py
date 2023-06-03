a7ad3455e5bec729d6a51efa20835cc3c5cabee6
zvdy <cristian.030902@gmail.com>
2023-06-04 00:01:25 +0200

    time needed to inform all employees

---
+++ time-needed-to-inform-all-employees.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
+            
+            # Create a graph
+            graph = defaultdict(list)
+            for i in range(n):
+                if manager[i] != -1:
+                    graph[manager[i]].append(i)
+            
+            # DFS
+            def dfs(node, time):
+                if not graph[node]:
+                    return time
+                else:
+                    return max(dfs(child, time + informTime[node]) for child in graph[node])
+            
+            return dfs(headID, 0)
