c498609e48f032a246235ed4afc5e7a3bb5768fa
zvdy <cristian.030902@gmail.com>
2023-06-04 17:39:40 +0200

    number of provinces

---
+++ number-of-provinces.py
@@ -0,0 +1,31 @@
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        
+        adj = defaultdict(list)
+        n = len(isConnected)
+        
+        for i in range(n):
+            for j in range(n):
+                if isConnected[i][j]:
+                    adj[i].append(j)
+        visited = set()
+        res = 0
+        
+        def bfs(i):
+            nonlocal res
+            q = deque([i])
+            visited.add(i)
+            while q:
+               node = q.popleft()
+               for nei in adj[node]:
+                   if nei not in visited:
+                       visited.add(nei)
+                       q.append(nei)
+            
+            res += 1            
+            
+        for i in range(n):
+            if i not in visited:
+                bfs(i)
+            
+        return res   
