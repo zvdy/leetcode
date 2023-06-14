9bf36fdd4314952c185c0543bae8c1e6185e8e97
zvdy <cristian.030902@gmail.com>
2023-06-14 22:57:10 +0200

    minimum absolute difference in bst

---
+++ minimum-absolute-difference-in-bst.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
+        def inorder(root):
+            if root:
+                inorder(root.left)
+                self.res = min(self.res, root.val - self.prev)
+                self.prev = root.val
+                inorder(root.right)
+                
+        self.prev = float('-inf')
+        self.res = float('inf')
+        inorder(root)
+        return self.res
