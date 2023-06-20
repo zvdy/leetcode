298a531960cfa3e380a42d5ba5d334cb20ce2006
zvdy <cristian.030902@gmail.com>
2023-06-20 22:36:42 +0200

    k radius subarray averages

---
+++ k-radius-subarray-averages.py
@@ -0,0 +1,16 @@
+class Solution:
+    def getAverages(self, nums: List[int], k: int) -> List[int]:
+        n = len(nums)
+        answer = [-1 for _ in range(n)]
+        curr_sum = 0
+        length = k+k+1
+        if length > n:
+            return answer
+        for i in range(0,length):
+            curr_sum+=nums[i]
+        answer[k] = curr_sum//length
+        for i in range(k+1,n-k):
+            curr_sum+=nums[i+k]
+            curr_sum-=nums[i-k-1]
+            answer[i] = curr_sum//length
+        return answer
