7b0cac6c860e27a2c7b5fce30d603bfcc8ec5c16
zvdy <cristian.030902@gmail.com>
2023-06-26 22:32:31 +0200

    minimum score of a path between two cities

---
+++ minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.py
@@ -0,0 +1,26 @@
+class Solution:
+    def minScore(self, n: int, roads: List[List[int]]) -> int:
+
+        paths = defaultdict(list) 
+        for x,y,l in roads:
+            paths[x].append((y,l))
+            paths[y].append((x,l))
+        visited = set()
+        
+        mini = 10**10
+        
+        q = []
+        q.append((1,10**10))
+        
+        while q:
+            cur, l = q.pop()
+            mini = min(mini,l) 
+            
+            if cur in visited:
+                continue
+
+            for nxt in paths[cur]:
+                q.append(nxt)
+            visited.add(cur)
+
+        return mini
