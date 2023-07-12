05ea92ba216e8652048886ff0127e35b04f1dbd7
zvdy <cristian.030902@gmail.com>
2023-07-12 22:01:56 +0200

    find eventual safe states

---
+++ find-eventual-safe-states.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isSafe(self, s, graph, visited, checkCycle):
+        if visited[s] == 1:
+            return True  
+        if visited[s] == -1:
+            return False  
+
+        visited[s] = -1 
+        for it in graph[s]:
+            if not self.isSafe(it, graph, visited, checkCycle):
+                return False 
+        visited[s] = 1  
+        return True
+
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+        v = len(graph)
+        visited = [0] * v
+        checkCycle = []
+        for i in range(v):
+            if self.isSafe(i, graph, visited, checkCycle):
+                checkCycle.append(i)  
+        return checkCycle
