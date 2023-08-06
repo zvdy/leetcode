a43dfba3182c2beee49256a91066d3547d2c6e03
zvdy <cristian.030902@gmail.com>
2023-08-06 21:00:59 +0200

    number of music playlists

---
+++ number-of-music-playlists.py
@@ -0,0 +1,10 @@
+class Solution:
+    def numMusicPlaylists(self, n: int, goal: int, k: int) -> int:
+        @lru_cache(None)
+        def dp(i, j):
+            if i == 0:
+                return +(j == 0)
+            ans = dp(i-1, j-1) * (n-j+1)
+            ans += dp(i-1, j) * max(j-k, 0)
+            return ans % (10**9 + 7)
+        return dp(goal, n)
