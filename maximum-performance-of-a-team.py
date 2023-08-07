a945392afd0e940ae456c72e1a1ce9628bffe419
zvdy <cristian.030902@gmail.com>
2023-08-07 12:57:52 +0200

    maximum performance of a team

---
+++ maximum-performance-of-a-team.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxPerformance(self, n: int, speed: List[int], efficiency: List[int], k: int) -> int:
+        # Sort by efficiency
+        engineers = sorted(zip(efficiency, speed), reverse=True)
+        speed_heap = []
+        speed_sum = 0
+        perf = 0
+        
+        for e, s in engineers:
+            if len(speed_heap) > k - 1:
+                speed_sum -= heapq.heappop(speed_heap)
+            heapq.heappush(speed_heap, s)
+            
+            speed_sum += s
+            perf = max(perf, speed_sum * e)
+            
+        return perf % (10**9 + 7)
