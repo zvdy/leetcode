27e208ed8b1f954af4605cef21021792fa5282ed
zvdy <cristian.030902@gmail.com>
2023-08-10 09:39:39 +0200

    search in rotated sorted array ii

---
+++ search-in-rotated-sorted-array-ii.py
@@ -0,0 +1,24 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        if not nums:
+            return False
+        l, r = 0, len(nums)-1
+        while l <= r:
+            while l < r and nums[l] == nums[l+1]:
+                l += 1
+            while l < r and nums[r] == nums[r-1]:
+                r -= 1
+            mid = (l+r)//2
+            if nums[mid] == target:
+                return True
+            if nums[mid] >= nums[l]:
+                if nums[l] <= target < nums[mid]:
+                    r = mid-1
+                else:
+                    l = mid+1
+            else:
+                if nums[mid] < target <= nums[r]:
+                    l = mid+1
+                else:
+                    r = mid-1
+        return False
