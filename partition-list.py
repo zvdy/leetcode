e901780e9ad30796663896cf9c0d36844fcc4fd6
zvdy <cristian.030902@gmail.com>
2023-08-15 07:01:55 +0200

    partition list

---
+++ partition-list.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]: 
+            if head is None:
+                return head
+            
+            left = ListNode()
+            right = ListNode()
+            
+            left_head = left
+            right_head = right
+            
+            while head:
+                if head.val < x:
+                    left.next = head
+                    left = left.next
+                else:
+                    right.next = head
+                    right = right.next
+                head = head.next
+            
+            right.next = None
+            left.next = right_head.next
+            
+            return left_head.next
