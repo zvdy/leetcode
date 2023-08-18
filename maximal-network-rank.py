3c87339cfbf29c2043c48aedee7c4200c9a68bdc
zvdy <cristian.030902@gmail.com>
2023-08-18 14:29:09 +0200

    maximal network rank

---
+++ maximal-network-rank.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maximalNetworkRank(self, n: int, roads: List[List[int]]) -> int:
+        graph = defaultdict(set)
+        for a, b in roads:
+            graph[a].add(b)
+            graph[b].add(a)
+        ans = 0
+        for i in range(n):
+            for j in range(i+1, n):
+                ans = max(ans, len(graph[i])+len(graph[j])-(j in graph[i]))
+        return ans
