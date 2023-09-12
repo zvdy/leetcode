2e5fbf76b1efa9deadffe1758a9aad90719ac63c
zvdy <cristian.030902@gmail.com>
2023-09-12 08:19:43 +0000

    minimum deletions to make character frequencies unique

---
+++ minimum-deletions-to-make-character-frequencies-unique.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minDeletions(self, s: str) -> int:
+        d = {}
+        for i in range(len(s)):
+            if s[i] not in d:
+                d[s[i]] = 1
+            else:
+                d[s[i]] += 1
+        res = 0
+        seen = set()
+        for key in d:
+            while d[key] in seen:
+                d[key] -= 1
+                res += 1
+            if d[key] != 0:
+                seen.add(d[key])
+        return res
