19a63fe5be58b7cd74d67b968b239078504bdc2e
zvdy <cristian.030902@gmail.com>
2023-09-22 15:09:29 +0200

    is subsequence

---
+++ is-subsequence.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        if len(s) == 0:
+            return True
+        if len(t) == 0:
+            return False
+        j = 0
+        for c in s:
+            i = t.find(c, j)
+            if i == -1:
+                return False
+            j = i + 1
+        return True
