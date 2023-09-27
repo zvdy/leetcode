4d6fc2054827cef291f8051984a088212db61d23
zvdy <cristian.030902@gmail.com>
2023-09-27 17:46:15 +0200

    decoded string at index

---
+++ decoded-string-at-index.py
@@ -0,0 +1,18 @@
+class Solution:
+    def decodeAtIndex(self, s: str, k: int) -> str:
+        n = len(s)
+        size = 0
+        for i in range(n):
+            if s[i].isdigit():
+                size *= int(s[i])
+            else:
+                size += 1
+        for i in range(n - 1, -1, -1):
+            k %= size
+            if k == 0 and s[i].isalpha():
+                return s[i]
+            if s[i].isdigit():
+                size //= int(s[i])
+            else:
+                size -= 1
+        return ''
