47f7c5d7609d9a885ea612147fa653c73a3b1a0b
zvdy <cristian.030902@gmail.com>
2023-10-07 22:30:52 +0200

    build array where you can find the maximum exactly k comparisons

---
+++ build-array-where-you-can-find-the-maximum-exactly-k-comparisons.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numOfArrays(self, n: int, m: int, k: int) -> int:
+        dp = [[[0 for _ in range(k+1)] for _ in range(m+1)] for _ in range(n+1)]
+        for i in range(1, m+1):
+            dp[1][i][1] = 1
+        for i in range(2, n+1):
+            for j in range(1, m+1):
+                for l in range(1, k+1):
+                    dp[i][j][l] = (dp[i][j][l] + dp[i-1][j][l]*j) % (10**9+7)
+                    for p in range(1, j):
+                        dp[i][j][l] = (dp[i][j][l] + dp[i-1][p][l-1]) % (10**9+7)
+        return sum(dp[n][i][k] for i in range(1, m+1)) % (10**9+7)
