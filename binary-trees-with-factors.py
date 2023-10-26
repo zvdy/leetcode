29b6d9d9cd06bd61cfd272b8d456c4ff49f470e7
zvdy <cristian.030902@gmail.com>
2023-10-26 18:45:03 +0200

    binary trees with factors

---
+++ binary-trees-with-factors.py
@@ -0,0 +1,18 @@
+class Solution:
+    def numFactoredBinaryTrees(self, arr: List[int]) -> int:
+        # Sort the array in ascending order
+        arr.sort()
+        # Create a dictionary to store the number of binary trees for each number in the array
+        dp = {}
+        # Iterate over the array
+        for i in range(len(arr)):
+            # Initialize the number of binary trees for the current number to 1
+            dp[arr[i]] = 1
+            # Iterate over the previous numbers in the array
+            for j in range(i):
+                # If the current number is divisible by a previous number and the quotient is also in the dictionary
+                if arr[i] % arr[j] == 0 and arr[i] // arr[j] in dp:
+                    # Add the product of the number of binary trees for the previous number and the quotient to the current number's count
+                    dp[arr[i]] += dp[arr[j]] * dp[arr[i] // arr[j]]
+        # Return the sum of the number of binary trees for all numbers in the array
+        return sum(dp.values()) % (10 ** 9 + 7)
