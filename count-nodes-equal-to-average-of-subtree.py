ca2565c4fee9a1770471cab8e5572f4efdfd6e48
zvdy <cristian.030902@gmail.com>
2023-11-02 10:45:40 +0100

    count nodes equal to average of subtree

---
+++ count-nodes-equal-to-average-of-subtree.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def averageOfSubtree(self, root: TreeNode) -> int:
+        ans = 0
+
+        def traverse(node):
+            nonlocal ans
+            
+            if not node:
+                return 0, 0
+            
+            # Traverse left and right subtrees
+            left_sum, left_count = traverse(node.left)
+            right_sum, right_count = traverse(node.right)
+            
+            # Calculate sum and count for current subtree
+            curr_sum = node.val + left_sum + right_sum
+            curr_count = 1 + left_count + right_count
+            
+            # Check if current subtree has average equal to node value
+            if curr_sum // curr_count == node.val:
+                ans += 1
+            
+            return curr_sum, curr_count
+        
+        # Start traversal from root
+        traverse(root)
+        return ans
