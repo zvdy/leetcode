b4f3e3be3e938c05158153fcd8f41be1e739b5f4
zvdy <cristian.030902@gmail.com>
2023-11-05 19:41:48 +0100

    find the winner of an array game

---
+++ find-the-winner-of-an-array-game.py
@@ -0,0 +1,22 @@
+class Solution:
+    def getWinner(self, arr: List[int], k: int) -> int:
+        # If k is greater than or equal to the length of the array, the maximum element is the winner
+        if k >= len(arr):
+            return max(arr)
+        
+        count = 0
+        # Keep looping until we have a winner
+        while count < k:
+            # If the first element is greater than the second element, move the second element to the end of the array
+            if arr[0] > arr[1]:
+                count += 1
+                arr.append(arr[1])
+                arr.pop(1)
+            # If the second element is greater than or equal to the first element, move the first element to the end of the array
+            else:
+                count = 1
+                arr.append(arr[0])
+                arr.pop(0)
+        
+        # Return the first element of the array, which is the winner
+        return arr[0]
