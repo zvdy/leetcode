624ec500a7376355f3076a1acb12e053864fb20d
zvdy <cristian.030902@gmail.com>
2023-11-13 18:20:36 +0100

    sort vowels in a string

---
+++ sort-vowels-in-a-string.py
@@ -0,0 +1,17 @@
+class Solution:
+    def sortVowels(self, s: str) -> str:
+        # Sort the vowels in descending order
+        vowels_sorted = sorted([c for c in s if c.lower() in 'aeiou'], reverse=True)
+
+        result = []
+        # Iterate through each character in the string
+        for char in s:
+            # If the character is a vowel, append the next largest vowel from the sorted list
+            if char.lower() in 'aeiou':
+                result.append(vowels_sorted.pop())
+            # Otherwise, append the character as is
+            else:
+                result.append(char)
+
+        # Join the resulting list of characters into a string and return it
+        return ''.join(result)
