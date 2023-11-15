627509974c3c79e7810677b257cb937fcc63e84c
zvdy <cristian.030902@gmail.com>
2023-11-15 18:20:29 +0100

    maximum element after decreasing and rearranging

---
+++ maximum-element-after-decreasing-and-rearranging.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maximumElementAfterDecrementingAndRearranging(self, arr: List[int]) -> int:
+        # Sort the array in ascending order
+        arr.sort()
+        # Set the first element to 1
+        arr[0] = 1
+        # Iterate through the array starting from the second element
+        for i in range(1, len(arr)):
+            # If the difference between the current element and the previous element is greater than 1,
+            # set the current element to the previous element plus 1
+            if arr[i] - arr[i - 1] > 1:
+                arr[i] = arr[i - 1] + 1
+        # Return the last element in the array
+        return arr[-1]
