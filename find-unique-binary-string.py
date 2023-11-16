91e7882792a00c1a2e0441fa85505b0320bec276
zvdy <cristian.030902@gmail.com>
2023-11-16 21:50:21 +0100

    find unique binary string

---
+++ find-unique-binary-string.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findDifferentBinaryString(self, nums: List[str]) -> str:
+        # Create a set of the input array
+        nums_set = set(nums)
+        # Iterate through all possible binary strings of length n
+        for i in range(2 ** len(nums)):
+            # Convert the integer to a binary string
+            binary_string = bin(i)[2:]
+            # Pad the binary string with leading zeros
+            binary_string = '0' * (len(nums) - len(binary_string)) + binary_string
+            # If the binary string is not in the set, return it
+            if binary_string not in nums_set:
+                return binary_string
