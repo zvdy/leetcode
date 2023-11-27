5c58288d2764455923c532dfb52af38a6daf35bf
zvdy <cristian.030902@gmail.com>
2023-11-27 13:34:04 +0100

    knight dialer

---
+++ knight-dialer.py
@@ -0,0 +1,24 @@
+class Solution:
+    def knightDialer(self, n: int) -> int:
+        MOD = 10**9 + 7
+        moves = {
+            1: [6, 8],
+            2: [7, 9],
+            3: [4, 8],
+            4: [3, 9, 0],
+            5: [],
+            6: [1, 7, 0],
+            7: [2, 6],
+            8: [1, 3],
+            9: [2, 4],
+            0: [4, 6]
+        }
+        dp = [1] * 10
+        for _ in range(n - 1):
+            dp2 = [0] * 10
+            for i in range(10):
+                for j in moves[i]:
+                    dp2[j] += dp[i]
+                    dp2[j] %= MOD
+            dp = dp2
+        return sum(dp) % MOD
