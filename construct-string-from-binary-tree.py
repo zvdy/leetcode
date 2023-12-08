be41801896ecbe89c94bbb51691a981049505f32
zvdy <cristian.030902@gmail.com>
2023-12-08 16:54:09 +0100

    construct string from binary tree

--- construct-string-from-binary-tree.py
+++ construct-string-from-binary-tree.py
@@ -7,9 +7,14 @@
 class Solution:
     def tree2str(self, root: Optional[TreeNode]) -> str:
         if root is None:
-            return ""
+            return ""  # If the root is None, return an empty string
+        
         if root.left is None and root.right is None:
-            return str(root.val)
+            return str(root.val)  # If the root has no children, return the value of the root as a string
+        
         if root.right is None:
+            # If the root has a left child but no right child, recursively convert the left subtree to a string
             return str(root.val) + "(" + self.tree2str(root.left) + ")"
+        
+        # If the root has both left and right children, recursively convert both subtrees to strings
         return str(root.val) + "(" + self.tree2str(root.left) + ")" + "(" + self.tree2str(root.right) + ")"
