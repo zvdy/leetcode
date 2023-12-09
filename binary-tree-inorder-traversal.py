9c04eb32ac8b4c4de06d1de828e8fa5c07cfff5a
zvdy <cristian.030902@gmail.com>
2023-12-09 13:44:05 +0100

    binary tree inorder traversal

---
+++ binary-tree-inorder-traversal.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        # Check if the root is None, return an empty list
+        if root is None:
+            return []
+        
+        stack = []  # Initialize a stack to store nodes
+        result = []  # Initialize a list to store the inorder traversal result
+        
+        while True:
+            while root is not None:
+                stack.append(root)  # Push the current node to the stack
+                root = root.left  # Move to the left child
+            
+            if len(stack) == 0:
+                return result  # If the stack is empty, return the result
+            
+            root = stack.pop()  # Pop the top node from the stack
+            result.append(root.val)  # Add the value of the popped node to the result
+            root = root.right  # Move to the right child
+        
+        # The function will never reach this point, as it always returns in the while loop
