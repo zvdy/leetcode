420264d38df9077c69d8dbe92a95a2b648d27c3b
zvdy <78762257+zvdy@users.noreply.github.com>
2023-12-18 22:38:04 +0100

    maximum product difference between two pairs

---
+++ maximum-product-difference-between-two-pairs.py
@@ -0,0 +1,6 @@
+class Solution:
+    def maxProductDifference(self, nums: List[int]) -> int:
+        # Sort the array.
+        nums.sort()
+        # Return the product of the last two elements minus the product of the first two elements.
+        return (nums[-1] * nums[-2]) - (nums[0] * nums[1])
