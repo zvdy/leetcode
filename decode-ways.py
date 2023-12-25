392fa73bb3a4deb80aae8541d106e9f7a43d82e2
zvdy <cristian.030902@gmail.com>
2023-12-25 11:45:15 +0100

    decode ways

--- decode-ways.py
+++ decode-ways.py
@@ -2,12 +2,17 @@ class Solution:
     def numDecodings(self, s: str) -> int:
         if s[0] == '0':
             return 0
-        dp = [0] * (len(s) + 1)
-        dp[0] = 1
-        dp[1] = 1
+        
+        dp = [0] * (len(s) + 1)  # Initialize a dynamic programming array to store the number of decodings
+        
+        dp[0] = 1  # There is only one way to decode an empty string
+        dp[1] = 1  # There is only one way to decode a string of length 1
+        
         for i in range(2, len(s) + 1):
             if s[i - 1] != '0':
-                dp[i] += dp[i - 1]
+                dp[i] += dp[i - 1]  # If the current digit is not zero, it can be decoded individually
+            
             if s[i - 2] == '1' or (s[i - 2] == '2' and s[i - 1] < '7'):
-                dp[i] += dp[i - 2]
-        return dp[len(s)]
+                dp[i] += dp[i - 2]  # If the current digit and the previous digit form a valid two-digit number, it can be decoded together
+        
+        return dp[len(s)]  # Return the number of decodings for the entire string
