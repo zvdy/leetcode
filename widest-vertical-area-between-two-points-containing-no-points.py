1551c23c3e288828262939e96851d53cb2dbcbb5
zvdy <78762257+zvdy@users.noreply.github.com>
2023-12-26 19:50:35 +0100

    widest vertical area between two points containing no points

--- widest-vertical-area-between-two-points-containing-no-points.py
+++ widest-vertical-area-between-two-points-containing-no-points.py
@@ -1,12 +1,26 @@
 class Solution:
-    def maxWidthOfVerticalArea(self, points: List[List[int]]) -> int:
-        # Sort the points based on their x-coordinate.
-        points.sort(key=lambda x: x[0])
-        # Initialize the maximum width to 0.
-        maxWidth = 0
-        # Iterate over the points.
-        for i in range(1, len(points)):
-            # Update the maximum width if the difference between the current and previous x-coordinate is greater than the current maximum width.
-            maxWidth = max(maxWidth, points[i][0] - points[i - 1][0])
-        # Return the maximum width.
-        return maxWidth
+    def numRollsToTarget(self, n: int, k: int, target: int) -> int:
+        # If the target is less than the number of dice or greater than the maximum possible sum, return 0
+        if target < n or target > n * k:
+            return 0
+
+        # Initialize a 2D dynamic programming table with size (n+1) x (target+1)
+        dp = [[0] * (target + 1) for _ in range(n + 1)]
+
+        # For one dice, the number of ways to get the sum i is 1 for all i in [1, min(k, target)]
+        for i in range(1, min(k + 1, target + 1)):
+            dp[1][i] = 1
+
+        # For each number of dice from 2 to n
+        for i in range(2, n + 1):
+            # For each possible sum from 1 to target
+            for j in range(1, target + 1):
+                # For each possible face value from 1 to min(k, j)
+                for l in range(1, min(k + 1, j)):
+                    # Add the number of ways to get the sum j - l with one less dice
+                    dp[i][j] += dp[i - 1][j - l]
+                # Take the result modulo 1e9 + 7 to prevent overflow
+                dp[i][j] %= 1000000007
+
+        # Return the number of ways to get the target sum with n dice
+        return dp[n][target]
