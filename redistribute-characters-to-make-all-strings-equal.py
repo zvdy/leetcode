58f188e433396e2195a96ce3eed0fa315bff2862
zvdy <78762257+zvdy@users.noreply.github.com>
2023-12-30 14:56:54 +0100

    redistribute characters to make all strings equal

---
+++ redistribute-characters-to-make-all-strings-equal.py
@@ -0,0 +1,20 @@
+class Solution:
+    def makeEqual(self, words: List[str]) -> bool:
+        # Create a dictionary to store the frequency of each character in the words
+        d = {}
+        for i in words:
+            for j in i:
+                if j in d:
+                    d[j] += 1
+                else:
+                    d[j] = 1
+
+        # Iterate over the dictionary
+        for i in d:
+            # If the frequency of the current character is not divisible by the number of words, return False
+            if d[i] % len(words) != 0:
+                return False
+            
+        # If we have not returned False by this point, return True
+        return True
+    
