63443f909759dff5ffffae2eeb97109c0c04baaf
zvdy <78762257+zvdy@users.noreply.github.com>
2024-01-04 23:03:41 +0100

    minimum number of operations to make array empty

---
+++ minimum-number-of-operations-to-make-array-empty.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        # Create a counter object that counts the occurrence of each number in the list
+        counter = Counter(nums)
+        
+        # Initialize the answer to 0
+        ans = 0
+        
+        # Iterate over the values of the counter (i.e., the counts of each number)
+        for c in counter.values():
+            # If any number appears only once, return -1 as per the problem statement
+            if c == 1: 
+                return -1
+            
+            # Add the ceiling of the count divided by 3 to the answer
+            # This is because each operation can decrease the count of a number by up to 3
+            ans += ceil(c / 3)
+        
+        # Return the total number of operations
+        return ans
