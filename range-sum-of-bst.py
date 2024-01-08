c19613a638b6e54330c0ec94e260a57c76f64e39
zvdy <cristian.030902@gmail.com>
2024-01-08 14:26:37 +0100

    range sum of bst

--- range-sum-of-bst.py
+++ range-sum-of-bst.py
@@ -4,12 +4,24 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        # Base case: if the root is None, return 0
         if root is None:
             return 0
+        
+        # If the value of the root is less than the lower bound,
+        # recursively call the function on the right subtree
         if root.val < low:
             return self.rangeSumBST(root.right, low, high)
+        
+        # If the value of the root is greater than the upper bound,
+        # recursively call the function on the left subtree
         if root.val > high:
             return self.rangeSumBST(root.left, low, high)
+        
+        # If the value of the root is within the range,
+        # recursively call the function on both the left and right subtrees,
+        # and return the sum of the root value and the results from the subtrees
         return root.val + self.rangeSumBST(root.left, low, high) + self.rangeSumBST(root.right, low, high)
