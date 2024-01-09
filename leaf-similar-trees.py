3a3b723a7c8608456cd0c4dba3cb8792145a85bd
zvdy <78762257+zvdy@users.noreply.github.com>
2024-01-09 15:38:14 +0100

    leaf similar trees

--- leaf-similar-trees.py
+++ leaf-similar-trees.py
@@ -4,12 +4,22 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        # Define a generator function for depth-first search (DFS)
         def dfs(node):
+            # If the node exists
             if node:
+                # If the node is a leaf (i.e., it has no children)
                 if not node.left and not node.right:
+                    # Yield the value of the leaf
                     yield node.val
+                # Recursively perform DFS on the left child
                 yield from dfs(node.left)
+                # Recursively perform DFS on the right child
                 yield from dfs(node.right)
+        
+        # Compare the leaf sequences of the two trees
+        # If they are the same, return True; otherwise, return False
         return list(dfs(root1)) == list(dfs(root2))
