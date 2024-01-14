ff5ae388fb1c92c2b9620c758e87a52f50200728
zvdy <78762257+zvdy@users.noreply.github.com>
2024-01-14 21:39:56 +0100

    determine-if-two-strings-are-close

--- determine-if-two-strings-are-close.py
+++ determine-if-two-strings-are-close.py
@@ -1,7 +1,11 @@
 class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
-        if len(word1) != len(word2):
-            return False
-        if set(word1) != set(word2):
-            return False
-        return sorted(Counter(word1).values()) == sorted(Counter(word2).values())
+        # If the lengths of the two words are not equal, they can't be close strings
+        if len(word1) != len(word2): return False
+
+        # Count the occurrences of each character in the two words
+        c1, c2 = Counter(word1), Counter(word2)
+
+        # Two strings are close if they have the same characters (regardless of order)
+        # and the same frequencies of characters (also regardless of order)
+        return sorted(c1.keys()) == sorted(c2.keys()) and sorted(c1.values()) == sorted(c2.values())
