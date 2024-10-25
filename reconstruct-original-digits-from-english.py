d365c77af0e5568565aabd450ab567a7ae584313
Cristian zvdy <cristian.030902@gmail.com>
2024-10-25 16:35:16 +0200

    reconstruct original digits from enlish

---
+++ reconstruct-original-digits-from-english.py
@@ -0,0 +1,30 @@
+from collections import Counter
+
+class Solution:
+    def originalDigits(self, s: str) -> str:
+        count = Counter(s)
+        
+        digits = [
+            (0, 'zero', 'z'),
+            (2, 'two', 'w'),
+            (4, 'four', 'u'),
+            (6, 'six', 'x'),
+            (8, 'eight', 'g'),
+            (1, 'one', 'o'),
+            (3, 'three', 'r'),
+            (5, 'five', 'f'),
+            (7, 'seven', 's'),
+            (9, 'nine', 'i'),
+        ]
+        
+        result = []
+        
+        for digit, word, char in digits:
+            while count[char] > 0:
+                result.append(str(digit))
+                
+                for c in word:
+                    count[c] -= 1
+        
+        return ''.join(sorted(result))
+    
