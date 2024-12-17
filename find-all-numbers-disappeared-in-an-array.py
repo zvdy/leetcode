173fbaa45cde1e2966f52a949e04c1ed962bd496
zvdy <cristian.030902@gmail.com>
2024-12-17 18:15:36 +0100

    find all numbers disappeared in an array

--- find-all-numbers-disappeared-in-an-array.py
+++ find-all-numbers-disappeared-in-an-array.py
@@ -1,17 +1,11 @@
 from typing import *
+
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-        uniq = set() # init  a set to store unique values to compare with the range of the list
-        result = [] 
+        uniq = set(nums)
+        result = []
 
-        # populate the set with unique values
-        for i in nums:
-            print(i)
-            uniq.add(i)
-
-        # compare the set with the range of the list and append the missing values to the result list
-        for j in range(1, len(nums) + 1): 
-            print(j)
+        for j in range(1, len(nums) + 1):
             if j not in uniq:
                 result.append(j)
 
