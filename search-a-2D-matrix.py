a2cff119ebfbaa401039ae2bc17629649be2b468
zvdy <cristian.030902@gmail.com>
2024-12-28 20:18:28 +0100

    search 2d matrix

--- search-a-2D-matrix.py
+++ search-a-2D-matrix.py
@@ -1,9 +1,32 @@
 from typing import *
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        if not matrix:
+        rows = len(matrix)
+        cols = len(matrix[0])
+
+        top, bottom = 0, rows - 1
+
+        while top <= bottom:
+            row = (top + bottom) // 2
+            if target > matrix[row][-1]:
+                top = row + 1
+            elif target < matrix[row][0]:
+                bottom = row - 1
+            else:
+                break
+
+        if not (top <= bottom):
             return False
-        for row in matrix:
-            if target in row:
+
+        row = (top + bottom) // 2
+        left, right = 0, cols - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if target > matrix[row][mid]:
+                left = mid + 1
+            elif target < matrix[row][mid]:
+                right = mid - 1
+            else:
                 return True
-        return False
+        return False  
+    
